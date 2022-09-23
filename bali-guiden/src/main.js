5abc0aa3c039bc1a61845ff4cd9b93577f9ce776
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-09-23 07:05:22 +0200

    add menu overlay functionality | fix cookie banner

--- bali-guiden/src/main.js
+++ bali-guiden/src/main.js
@@ -1,8 +1,22 @@
 const cookieButtons = document.querySelectorAll('.cookie-button');
+const formButton = document.querySelector('#newsletter button');
+const hamburgerButton = document.querySelector('#hamburger-button');
+const closeButton = document.querySelector('#close-button');
 const cookieBanner = document.querySelector('#cookie-banner');
-console.log(cookieButtons);
+const nav = document.querySelector('nav');
+
+formButton.disabled = true;
+
 cookieButtons.forEach(button => {
   button.addEventListener('click', () => {
-    cookieBanner.classList.add('cookie-banner-read');
+    cookieBanner.style.display = 'none';
   });
+});
+
+hamburgerButton.addEventListener('click', () => {
+  nav.style.transform = 'translateX(0)';
+});
+
+closeButton.addEventListener('click', () => {
+  nav.style.transform = 'translateX(100%)';
 });
