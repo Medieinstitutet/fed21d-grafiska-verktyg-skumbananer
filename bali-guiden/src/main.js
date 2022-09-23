0351a90417325851487c4f58a35569fd0d1f9d94
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-09-23 04:28:09 +0200

    tablet & desktop done | cookie banner functionality done

--- bali-guiden/src/main.js
+++ bali-guiden/src/main.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+const cookieButtons = document.querySelectorAll('.cookie-button');
+const cookieBanner = document.querySelector('#cookie-banner');
+console.log(cookieButtons);
+cookieButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    cookieBanner.classList.add('cookie-banner-read');
+  });
+});
