0445b4ccbf4875e2be165be5ab12ce7f0c0bc9cd
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-09-24 22:54:24 +0200

    clean up JavaScript

--- bali-guiden/src/main.js
+++ bali-guiden/src/main.js
@@ -1,22 +1,13 @@
 const cookieButtons = document.querySelectorAll('.cookie-button');
-const formButton = document.querySelector('.newsletter button');
-const hamburgerButton = document.querySelector('.hamburger-button');
+const burgerButton = document.querySelector('.hamburger-button');
 const closeButton = document.querySelector('.close-button');
 const cookieBanner = document.querySelector('.cookie-banner');
 const nav = document.querySelector('nav');
 
-formButton.disabled = true;
+cookieButtons.forEach(btn =>
+  btn.addEventListener('click', () => (cookieBanner.style.display = 'none'))
+);
 
-cookieButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    cookieBanner.style.display = 'none';
-  });
-});
+burgerButton.addEventListener('click', () => nav.classList.toggle('is-active'));
 
-hamburgerButton.addEventListener('click', () => {
-  nav.classList.toggle('is-active');
-});
-
-closeButton.addEventListener('click', () => {
-  nav.classList.toggle('is-active');
-});
+closeButton.addEventListener('click', () => nav.classList.toggle('is-active'));
