bdbf8766d86f2b27687116a11a5f302b3b02ef6d
Malin Johansson <malin.johansson@medieinstitutet.se>
2022-10-02 00:43:26 +0200

    desktop and menu done

--- champs-hunddagis/champs-hunddagis-code/main.js
+++ champs-hunddagis/champs-hunddagis-code/main.js
@@ -1,1 +1,9 @@
 import './scss/style.scss';
+
+const burgerMenu = document.getElementById("hamburger-menu");
+const menu = document.getElementById("navbar");
+
+burgerMenu.addEventListener("click", function() {
+    this.classList.toggle("open");
+    menu.classList.toggle("open-menu")
+});
