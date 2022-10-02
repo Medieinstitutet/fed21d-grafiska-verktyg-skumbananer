9f03ebed01bb4e5d7bb89c16784e5ff77e012a9b
Malin Johansson <malin.johansson@medieinstitutet.se>
2022-10-02 21:07:15 +0200

    cookie banner done

--- champs-hunddagis/champs-hunddagis-code/main.js
+++ champs-hunddagis/champs-hunddagis-code/main.js
@@ -2,8 +2,18 @@ import './scss/style.scss';
 
 const burgerMenu = document.getElementById("hamburger-menu");
 const menu = document.getElementById("navbar");
+const acceptBtn = document.getElementById("accept-btn");
+const rejectBtn = document.getElementById("reject-btn");
+const cookieBanner = document.getElementById("cookie-banner");
+
+function closeCookieBanner() {
+    cookieBanner.classList.toggle("close-cookie-banner");
+};
 
 burgerMenu.addEventListener("click", function() {
     this.classList.toggle("open");
-    menu.classList.toggle("open-menu")
+    menu.classList.toggle("open-menu");
 });
+
+acceptBtn.addEventListener("click", closeCookieBanner);
+rejectBtn.addEventListener("click", closeCookieBanner);
